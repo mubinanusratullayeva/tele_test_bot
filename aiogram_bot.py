ed81453327bd550feb8447d221f3b8f1053bea48
smb <mubinanusratullayeva1@gmail.com>
2024-06-12 11:06:42 +0500

    getting data from db

--- aiogram_bot.py
+++ aiogram_bot.py
@@ -22,16 +22,30 @@ dp = Dispatcher(bot)
 async def send_welcome(message: types.Message):
     first_name = message.from_user.first_name
     username = message.from_user.username
-    print(f"{username} start bot")
-    query = f"""INSERT INTO users(first_name, username) VALUES('{first_name}', '{username}')"""
-    print(f"{username} {Database.connect(query, 'insert')} database")
+    chat_id = str(message.chat.id)
 
-    await message.reply(f"Hello, @{username}!")
+    check_query = f"""SELECT * FROM users WHERE chat_id = '{chat_id}'"""
+    if len(Database.connect(check_query, "select")) >= 1:
+        await message.reply(f"Hello, @{username}!")
+
+    else:
+        print(f"{username} start bot")
+        query = f"""INSERT INTO users(first_name, username) VALUES('{first_name}', '{username}')"""
+        print(f"{username} {Database.connect(query, 'insert')} database")
+        await message.reply(f"Hello, @{username}!")
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+# @dp.message_handler()
+# async def echo(message: types.Message):
+#     await message.answer(message.text)
+
+
+@dp.message_handler(commands=["data"])
+async def select(message: types.Message):
+    chat_id = message.chat.id
+    query_select = f"SELECT * FROM users"
+    data = Database.connect(query_select, "select")
+    await message.reply(f"{data}")
 
 
 if __name__ == '__main__':
